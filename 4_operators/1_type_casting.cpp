8c7c62456abdeb24df1b6bf5444cd9b5c604004b
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-09-23 21:45:29 +0530

    23-09-2025

---
+++ 4_operators/1_type_casting.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x=5;
+    float y=x;
+
+    // cout<<"x:"<<x<<endl;
+    // cout<<"y:"<<y<<endl;
+
+    // bool flag=true;
+
+    // cout<<flag+10<<endl;
+
+    // cout<<flag;
+
+    //#EXPLICIT TYPE CASTING
+    //int a=5;
+    int b=2;
+
+    // cout<<a/b<<endl;
+    // cout<<(float)a/b<<endl;
+
+    // float pi=3.14;
+    // cout<<(int)pi;
+
+
+    char c='A';
+    int a=1;
+
+    cout<<(char)(a+c)<<endl;
+    return 0;
+}
---
+++ 4_operators/2_pre_and_post_increment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=0;
+
+    cout<<i++<<endl;
+    cout<<++i;
+
+    return 0;
+}

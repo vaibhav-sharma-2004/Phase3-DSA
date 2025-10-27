4082f47f2c97b3645005db6fcc78a39343d71f92
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-10-27 21:50:37 +0530

    27-10-2025

---
+++ 11_vectors/4_binary_search.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> a={1,2,5,10,20,50,100,400};
+
+    int start=0;
+    int end=a.size()-1;
+
+    int key=400;
+
+
+    while(start<=end){
+
+        int mid=(start+end)/2;
+
+        if(key>a[mid]){//right
+            start=mid+1;
+        }
+
+        else if(key<a[mid]){
+            end=mid-1;
+        }
+
+        else{
+
+            cout<<"key is present at:"<<mid<<endl;
+            break;
+        }
+    }
+
+    if(start>end){
+        cout<<"key is not present"<<endl;
+    }
+
+    return 0;
+}
